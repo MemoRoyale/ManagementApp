135a6fe24a5975b840d13b45cdcb3c88458eef5c
A Smoker <a6b7c@hotmail.com>
2024-09-21 01:17:00 +0800

    Add Images Add Redux Toolkit Add States api and state index

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { LockIcon } from 'lucide-react';
+import Image from 'next/image';
 import React, { useState } from 'react'
 
 
@@ -15,11 +17,24 @@ const Sidebar = () => {
         <div className='flex h-[100%] w--full flex-col justify-start'>
             <div className='z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6
             pt-3 dark:bg-black'>
-               <div className='text-xl font-bold text-gray-800 dark:text-white'> BEETASK
+               <div className='text-xl font-bold text-gray-800 dark:text-white'>
+                 BEETASK
                 </div> 
-
             </div>
-
+              {/* TEAM */}
+              <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700'>
+                <Image src="/logo.png" alt="Logo" width={40} height={40} />
+                <div className=''>
+                  <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>
+                    EDROH TEAM
+                  </h3>
+                  <div className='mt-1 flex items-start gap-2 '>
+                    <LockIcon className='mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400'/>
+                    <p className='text-xs text-gray-500'>Private</p>
+                  </div>
+                </div>
+              </div>
+              {/* NAVAR LINKS */}
         </div>
     </div>
   )
--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import Navbar from "@/app/(components)/Navbar"
 import Sidebar from "@/app/(components)/Sidebar"
+import StoreProvider, { useAppSelector } from './redux';
 
-const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
+const DashboardLayout = ({children}:{children:React.ReactNode}) => {
+  const isSidebarCollapsed= useAppSelector((state)=>state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state)=> state.global.isDarkMode);
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+    }else{
+      document.documentElement.classList.remove('dark');
+    }
+  })
   return  (
   <div className='flex min-h-screen w-full bg-gray-50 text-gray-500'>
 <Sidebar/>
-<main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+<main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+  isSidebarCollapsed? "":"md:pl-64"} `}
 >
     {/*navbar*/}
     <Navbar/>
@@ -15,5 +27,11 @@ const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
 </div>
   );
 }
-
+const DashboardWrapper = ({children}:{children:React.ReactNode})=>{
+    return(
+      <StoreProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </StoreProvider>
+    )
+}
 export default DashboardWrapper
