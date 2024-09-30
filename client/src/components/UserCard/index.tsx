f752697150d0df821ab2784b547bebabb0d9f0f5
A Smoker <a6b7c@hotmail.com>
2024-10-01 07:38:49 +0800

    Fix Sreach Users images URl

--- client/src/components/UserCard/index.tsx
+++ client/src/components/UserCard/index.tsx
@@ -11,7 +11,7 @@ const UserCard = ({ user }: Props) => {
     <div className="flex items-center rounded border p-4 shadow">
       {user.profilePictureUrl && (
         <Image
-          src={`https://pmapp-s3-images.s3.ap-southeast-2.amazonaws.com/`}
+          src={`https://pmapp-s3-images.s3.ap-southeast-2.amazonaws.com/${user.profilePictureUrl}`}
           alt="profile picture"
           width={32}
           height={32}
